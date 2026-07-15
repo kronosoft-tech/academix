24ead53ebd0252f3765d9bcf300a42743ef1f43d
luiferDev <luifer991@gmail.com>
2026-07-14 22:17:03 -0500

    refactor(accounting): update components barrel export
    Remove exports for deleted components (EquityForm, LiabilityForm,
    FixedAssetForm, PDFGenerator).

--- src/features/accounting/components/index.ts
+++ src/features/accounting/components/index.ts
@@ -1,15 +1,12 @@
-// Accounting Module Components Export - Phase 13
+// Accounting Module Components
 
 export { SkeletonTable, SkeletonCard, SkeletonChart, SkeletonForm } from "./SkeletonTable";
-export { EntriesTable, EmployeesTable, PayrollRunsTable, PayrollEntriesTable } from "./AccountingTable";
+export { AccountingTable } from "./AccountingTable";
 export { IncomeForm } from "./IncomeForm";
 export { ExpenseForm } from "./ExpenseForm";
+export { DashboardCards } from "./DashboardCards";
 export {
   IncomeExpensesChart,
   MonthlyTrendChart,
   ExpenseBreakdownChart,
-  IncomeByCategoryChart,
-  ProfitMarginChart,
-  PayrollSummaryChart,
 } from "./DashboardCharts";
-export * from "./PDFGenerator";
